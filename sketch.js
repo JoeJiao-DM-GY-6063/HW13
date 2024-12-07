f34ba347dac155b8cd560e4039b732e4d2cbf474
Joe <Joeao1026@gmail.com>
2024-12-06 22:50:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,85 +1,81 @@
-// serial variables
+// 串口变量
 let mSerial;
-
 let connectButton;
+let readyToReceive = false;
 
-let readyToReceive;
+// 火柴状态
+let state = "OFF"; // 火柴状态：ON 或 OFF
+let md = 0;        // 火焰大小
+let mr = 0;        // 火焰颜色
 
-// project variables
-let mElls = [];
-
+// 串口数据处理函数
 function receiveSerial() {
   let line = mSerial.readUntil("\n");
-  trim(line);
+  line = trim(line); // 去除多余空格
   if (!line) return;
 
-  if (line.charAt(0) != "{") {
-    print("error: ", line);
-    readyToReceive = true;
-    return;
+  // 数据解析
+  if (line === "ON") {
+    state = "ON"; // 收到 ON 指令，保持点亮状态
+  } else if (line === "OFF") {
+    state = "OFF"; // 收到 OFF 指令，保持熄灭状态
   }
 
-  // get data from Serial string
-  let data = JSON.parse(line).data;
-  let a0 = data.A0;
-  let d2 = data.D2;
-
-  // use data to update project variables
-  if (d2.isPressed) {
-    mElls.push({
-      x: random(width),
-      y: random(height),
-      c: map(d2.count % 20, 0, 20, 155, 255),
-      d: map(a0.value, 0, 4095, 20, 200),
-    });
-  }
-
-  // serial update
+  // 准备接收下一条数据
   readyToReceive = true;
 }
 
+// 串口连接函数
 function connectToSerial() {
   if (!mSerial.opened()) {
     mSerial.open(9600);
-
     readyToReceive = true;
     connectButton.hide();
   }
 }
 
 function setup() {
-  // setup project
+  rectMode(CENTER);
   createCanvas(windowWidth, windowHeight);
 
-  // setup serial
-  readyToReceive = false;
-
+  // 初始化串口
   mSerial = createSerial();
 
-  connectButton = createButton("Connect To Serial");
-  connectButton.position(width / 2, height / 2);
+  // 串口按钮
+  connectButton = createButton("连接串口");
+  connectButton.position(width / 2 - 50, height / 2);
   connectButton.mousePressed(connectToSerial);
 }
 
 function draw() {
-  // project logic
-  background(0);
+  background(31, 32, 36);
 
-  for (let i = 0; i < mElls.length; i++) {
-    let me = mElls[i];
-    fill(me.c, 0, 0);
-    ellipse(me.x, me.y, me.d, me.d);
+  // 绘制火柴杆
+  fill(64, 53, 40);
+  rect(width / 2, height - 250, 30, 500);
+
+    // 绘制火焰
+    noStroke();
+    fill(mr, 150, 50);
+    ellipse(width / 2, height - 520, md);
+
+  // 绘制火柴头
+  fill(state === "ON" ? 245 : 69, 100, 81);
+  ellipse(width / 2, height - 500, 40, 60);
+
+
+
+  // 更新火焰状态
+  if (state === "ON") {
+    md = lerp(md+30, 150, 0.1); // 火焰逐渐增大
+    mr = lerp(mr, 255, 0.1); // 火焰变亮
+  } else {
+    md = lerp(md, 0, 0.1);   // 火焰逐渐减小
+    mr = lerp(mr, 0, 0.1);   // 火焰变暗
   }
 
-  // update serial: request new data
-  if (mSerial.opened() && readyToReceive) {
-    readyToReceive = false;
-    mSerial.clear();
-    mSerial.write(0xab);
-  }
-
-  // update serial: read new data
-  if (mSerial.availableBytes() > 8) {
+  // 串口数据读取
+  if (mSerial.opened() && mSerial.availableBytes() > 0) {
     receiveSerial();
   }
 }
